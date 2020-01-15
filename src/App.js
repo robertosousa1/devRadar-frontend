86144860e63a2da44f25361f28ff50e719cff66c
Roberto <roberto_junior_sousa@hotmail.com>
2020-01-15 15:34:10 -0300

    estrutura básica para a aplicação

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/17255765?s=400&u=3f3723d564c8ae3670f8931a73565c2120cdb3cd&v=4" alt="Roberto Alves" />
+              <div className="user-info">
+                <strong>Roberto Alves</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Engineer/Developer, pursuing a bachelor's in Software Engineering and Artificial Intelligence. Data Science enthusiastic.</p>
+            <a href="https://github.com/robertosousa1">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/17255765?s=400&u=3f3723d564c8ae3670f8931a73565c2120cdb3cd&v=4" alt="Roberto Alves" />
+              <div className="user-info">
+                <strong>Roberto Alves</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Engineer/Developer, pursuing a bachelor's in Software Engineering and Artificial Intelligence. Data Science enthusiastic.</p>
+            <a href="https://github.com/robertosousa1">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/17255765?s=400&u=3f3723d564c8ae3670f8931a73565c2120cdb3cd&v=4" alt="Roberto Alves" />
+              <div className="user-info">
+                <strong>Roberto Alves</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Engineer/Developer, pursuing a bachelor's in Software Engineering and Artificial Intelligence. Data Science enthusiastic.</p>
+            <a href="https://github.com/robertosousa1">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/17255765?s=400&u=3f3723d564c8ae3670f8931a73565c2120cdb3cd&v=4" alt="Roberto Alves" />
+              <div className="user-info">
+                <strong>Roberto Alves</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Engineer/Developer, pursuing a bachelor's in Software Engineering and Artificial Intelligence. Data Science enthusiastic.</p>
+            <a href="https://github.com/robertosousa1">Acessar perfil no Github</a>
+          </li>
+        </ul>          
+      </main>
     </div>
   );
 }
